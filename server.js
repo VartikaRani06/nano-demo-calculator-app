5bca004e9e6efcae4895d8c0dbb5a181f0c2b88e
VartikaRani06 <vartika.1si20ec106@gmail>
2023-06-01 00:07:43 +0530

    second commit

--- server.js
+++ server.js
@@ -10,17 +10,19 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
-});
-
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
-});
-
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
-});
+    return res.status(200).send('Hello world!');
+  });
+  
+  baseRouter.post('/add', (req, res) => {
+      var {first,second} = req.body;
+      res.status(200).json({ "result":  first+second});
+  });
+  
+  
+  baseRouter.post('/subtract', (req, res) => {
+      var {first,second} = req.body;
+      res.json({ "result": first-second });
+  });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
